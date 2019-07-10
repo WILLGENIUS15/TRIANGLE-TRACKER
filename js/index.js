463ec3991600dd9ccfab49d3310b93ea25b3c849
fidel omondi <willgenius@gmail.com>
2019-07-10 10:09:38 +0300

    adding functions

--- js/index.js
+++ js/index.js
@@ -11,3 +11,6 @@ if ((inp11 === inp22) && (inp11 === inp33) && (inp22 === inp33)) {
   else if ((inp11 === inp22) ||(inp11 === inp33) || (inp22 === inp33)) {
   alert("Isosceles");
   }
+  else if (((inp11!=inp22)&&(inp11!=inp33)&&(inp22!=inp33))||((inp11+inp22>inp33)||(inp11+inp33>inp22)||(inp22+inp33>inp1))) {
+  alert("scalene");
+  }
