e733c208192337975839ba6556dbe32fa22bc951
fidel omondi <willgenius@gmail.com>
2019-07-10 10:07:04 +0300

    adding functions

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ function fidel(){
 var inp1 = document.getElementById("Triangle_Input1").value;
 var inp2 = document.getElementById("Triangle_Input2").value;
 var inp3 = document.getElementById("Triangle_Input3").value;
+var inp11=parseInt(inp1);
+var inp22=parseInt(inp2);
+var inp33=parseInt(inp3);
